7cea5c808da03ded7b50669ca728f19398e4dd07
Ubitroll <stephencartner@hotmail.co.uk>
2023-03-17 14:56:21 +0000

    Server and Client communication

---
+++ SocketProject/SocketClient/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket client = null;
+
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPAddress ipAddr = null;
+
+            try
+            {
+                Console.WriteLine("*** Welcome to Socket Client Starter ***");
+                Console.WriteLine("Please Type a Valid Server IP Address and Press Enter: ");
+
+                string striIpAddr = Console.ReadLine();
+
+                Console.WriteLine("Please Supply a Valid Port Number 0 - 65535 and Press Enter: ");
+                
+                string strPortInput = Console.ReadLine();
+
+                int nPortInput = 0;
+
+                if (striIpAddr == " ") striIpAddr= "127.0.0.1";
+                if (strPortInput == " ") strPortInput = "25000";
+
+                if (!IPAddress.TryParse(striIpAddr, out ipAddr))
+                {
+                    Console.WriteLine("Invalid server IP supplied.");
+                    return;
+                }
+
+                if(!int.TryParse(strPortInput.Trim(), out nPortInput))
+                {
+                    Console.WriteLine("Invalid port number Supplied.");
+                    return;
+                }
+
+                if (nPortInput <= 0 || nPortInput > 65535)
+                {
+                    Console.WriteLine("Port number must be between 0 and 65535");
+                    return;
+                }
+
+                System.Console.WriteLine(string.Format("IPAddress: {0} - Port: {1}", ipAddr.ToString(), nPortInput));
+
+                client.Connect(ipAddr, nPortInput);
+
+                Console.WriteLine("Connected to the server, type text and press enter to send it to the server, type <EXIT> to close.");
+
+                string inputCommand = string.Empty;
+
+                while (true)
+                {
+                    inputCommand = Console.ReadLine();
+
+                    if (inputCommand.Equals("<EXIT>"))
+                    {
+                        break;
+                    }
+
+                    byte[] buffSend = Encoding.ASCII.GetBytes(inputCommand);
+
+                    client.Send(buffSend);
+
+                    byte[] buffReceived = new byte[128];
+                    int nRecv = client.Receive(buffReceived);
+
+                    Console.WriteLine("Data Received : {0}",Encoding.ASCII.GetString(buffReceived, 0, nRecv));
+
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                if (client != null)
+                {
+                    if(client.Connected)
+                    {
+                        client.Shutdown(SocketShutdown.Both);
+                    }
+
+                    client.Close();
+                    client.Dispose();
+                }
+            }
+
+            Console.WriteLine("Press a key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SocketProject/SocketServer/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an object of Socket Class
+            Socket listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            // Create an object of an IP Address.Socket listening on any ip Address
+            IPAddress ipAddr = IPAddress.Any;
+
+            // Define IP ENDP POINT
+            IPEndPoint ipep = new IPEndPoint(ipAddr, 25000);
+
+            // Bind socket to up end point
+            try 
+            {
+                listenerSocket.Bind(ipep);
+
+                // Call listen method on the listener socket object, pass a number to signify how many
+                // Clients can wait for a connection while the system is busy handling another connection
+                listenerSocket.Listen(5);
+
+                // Call accept method on the listener socket.
+                // Accept is a synchronous method (blocking method() will not move forward until operation completed -- very bad method
+                Socket client = listenerSocket.Accept();
+
+                // print out client ip end point
+                Console.WriteLine("Client connected: " + client.ToString() + " - IP End Point: " + client.RemoteEndPoint.ToString());
+
+                // Define buffer as type array
+                byte[] buffer = new byte[128];
+
+                // Define number of received bytes as an int
+                int numberOfReceivedBytes = 0;
+                
+                while (true)
+                {
+                    numberOfReceivedBytes = client.Receive(buffer);
+                    Console.WriteLine("Number of received bytes: " + numberOfReceivedBytes);
+
+                    // Convert from byte array to ascii , using encoding.ASCII.GetString
+                    string receivedText = Encoding.ASCII.GetString(buffer, 0, numberOfReceivedBytes);
+
+                    // print out text
+                    Console.WriteLine("Data sent by client is: " + receivedText);
+
+                    // send data back to sender
+                    client.Send(buffer);
+
+                    if (receivedText == "x")
+                    {
+                        break;
+                    }
+
+                    Array.Clear(buffer, 0, numberOfReceivedBytes);
+                    numberOfReceivedBytes = 0;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
